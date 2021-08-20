70f448db2c691d5460faee67463d8a24f8cc74c4
murilonerdx <mu-silva@outlook.com>
2021-08-20 15:29:50 -0300

    Criando controller

---
+++ src/main/java/com/murilonerdx/restapispring/controller/PersonController.java
@@ -0,0 +1,51 @@
+package com.murilonerdx.restapispring.controller;
+
+import com.murilonerdx.restapispring.model.Person;
+import com.murilonerdx.restapispring.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonService service;
+
+    @GetMapping
+    public List<Person> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Person findById(@PathVariable("id") Long id) {
+        return service.findById(id);
+    }
+
+    @PostMapping
+    public Person create(@RequestBody Person person) {
+        return service.create(person);
+    }
+
+    @PutMapping
+    public Person update(@RequestBody Person person) {
+        return service.update(person);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        service.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
