c8a44b8ad83a4dd36cee8341323a027cb100fd50
murilonerdx <mu-silva@outlook.com>
2021-08-20 17:09:41 -0300

    Corrigindo bugs, adicionando header parameter e serialização yaml e yml

---
+++ src/main/java/com/murilonerdx/restapispring/config/YamlJackson2HttpMessageConverter.java
@@ -0,0 +1,13 @@
+package com.murilonerdx.restapispring.config;
+
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+
+public final class YamlJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter {
+
+    public YamlJackson2HttpMessageConverter() {
+        super(new YAMLMapper(), MediaType.parseMediaType("application/x-yaml"));
+    }
+
+}
--- src/main/java/com/murilonerdx/restapispring/controller/PersonController.java
+++ src/main/java/com/murilonerdx/restapispring/controller/PersonController.java
@@ -16,32 +16,30 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person")
 public class PersonController {
 
-    private final PersonService service;
-
     @Autowired
-    public PersonController(PersonService service) {
-        this.service = service;
-    }
+    private PersonService service;
 
-    @GetMapping(produces = {"application/json", "application/xml"})
+    @GetMapping(produces = { "application/json", "application/xml", "application/x-yaml" })
     public List<PersonDTO> findAll() {
         return service.findAll();
     }
 
-    @GetMapping(value= "/{id}", produces = {"application/json", "application/xml"})
+    @GetMapping(value = "/{id}", produces = { "application/json", "application/xml", "application/x-yaml" })
     public PersonDTO findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
 
-    @PostMapping(produces = {"application/json", "application/xml"}, consumes = {"application/json", "application/xml"})
+    @PostMapping(produces = { "application/json", "application/xml", "application/x-yaml" },
+            consumes = { "application/json", "application/xml", "application/x-yaml" })
     public PersonDTO create(@RequestBody PersonDTO person) {
         return service.create(person);
     }
 
-    @PutMapping(produces = {"application/json", "application/xml"}, consumes = {"application/json", "application/xml"})
+    @PutMapping(produces = { "application/json", "application/xml", "application/x-yaml" },
+            consumes = { "application/json", "application/xml", "application/x-yaml" })
     public PersonDTO update(@RequestBody PersonDTO person) {
         return service.update(person);
     }
@@ -51,5 +49,6 @@ public class PersonController {
         service.delete(id);
         return ResponseEntity.ok().build();
     }
+
 
 }
