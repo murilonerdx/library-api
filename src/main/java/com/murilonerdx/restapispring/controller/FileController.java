7bff6a752cc706abaae3fd563136d77425ab1751
murilonerdx <mu-silva@outlook.com>
2021-08-23 18:46:36 -0300

    Implementando download de arquivos

--- src/main/java/com/murilonerdx/restapispring/controller/FileController.java
+++ src/main/java/com/murilonerdx/restapispring/controller/FileController.java
@@ -1,18 +1,21 @@
 package com.murilonerdx.restapispring.controller;
 
 import com.murilonerdx.restapispring.dto.UploadFileResponseDTO;
+import com.murilonerdx.restapispring.exceptions.MyFileNotFoundException;
 import com.murilonerdx.restapispring.service.FileStorageService;
 import io.swagger.annotations.Api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -45,4 +48,24 @@ public class FileController {
                 .map(this::uploadFile)
                 .collect(Collectors.toList());
     }
+
+    @GetMapping("/downloadFile/{file:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String file, HttpServletRequest request){
+        Resource resource = fileStorageService.loadFileAsResource(file);
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(String.valueOf(resource.getFile().getAbsoluteFile()));
+        }catch(Exception e){
+            e.getMessage();
+        }
+        if(contentType == null){
+            contentType = "application/octet-stream";
+        }
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+
+
 }
