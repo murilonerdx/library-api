261e5fb9c48cb786ac9e4b7968d9b844ccaecae9
murilonerdx <mu-silva@outlook.com>
2021-08-23 15:19:37 -0300

    Adicionando UploadFile, e suas exceptions

---
+++ src/main/java/com/murilonerdx/restapispring/dto/UploadFileResponseDTO.java
@@ -0,0 +1,17 @@
+package com.murilonerdx.restapispring.dto;
+
+import com.github.dozermapper.core.Mapping;
+import lombok.Data;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+public class UploadFileResponseDTO implements Serializable {
+    @Mapping("id")
+    private String fileName;
+    private String fileDownloadUri;
+    private Date fileType;
+    private long size;
+}
